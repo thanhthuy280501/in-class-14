6c386200dbfd528f7e6b5b946774b51463e94fec
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-11-26 13:04:51 -0500

    Update evdemo.cpp

--- evdemo.cpp
+++ evdemo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #include "ev.h"			// You must build this.
 
 int main() {
-  Vehicle *transportationThing = new Vehicle;
+  Vehicle *transportationThing = new EV;
   Vehicle *model3 = new EV;
   Vehicle *mustang = new IC;
 
