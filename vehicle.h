1f92254c304d7e560b1c192ba7ab6e1ab773763a
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-11-26 13:03:49 -0500

    Update vehicle.h

--- vehicle.h
+++ vehicle.h
@@ -5,6 +5,8 @@ class Vehicle
   public:
     virtual void fillUp(int e) {
       std::cout << "Filling with " << e << " generic units of energy fuel." << endl;
+    virtual void fillUp(int e)=0;
+      
     }
 private:
 int gasCapacity;
