05c4f9dec77044dcb4468c465f56cb5eaee07bf0
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-11-26 13:08:38 -0500

    Update eve.h

--- eve.h
+++ eve.h
@@ -3,9 +3,15 @@
 class EV: public Vehicle
 {
   public:
+EV(int cc =10) : currCharge(cc){          //A zero and one paramater constructor
+}
+  
     virtual void fillUp(int e) {
       std::cout << "Filling with " << e << " generic units of energy fuel." << endl;
+      currCharge += e;
     }
+  private :
+int currCharge;
 
 };
 
